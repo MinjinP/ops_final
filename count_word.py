855b379433272914ee229b5d67e20baad074a754
hyewonm <42796947+hyewonm@users.noreply.github.com>
2020-06-28 15:09:01 +0900

    Update count_word.py

--- count_word.py
+++ count_word.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
 import re
@@ -18,59 +18,62 @@ app = Flask(__name__)
 def index():
     return render_template('final.html')
 
-@app.route('/final', methods=['POST'])
-    
-    if request.method == 'POST'
-	ht = request.form['ht']
+@app.route('/final', methods=['POST'])   
+def post(num=None):
 
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
 
-es_host="127.0.0.1"
-es_port="9200"
+        htm = request.args.get('ht')
 
-if __name__ == '__main__':
+        res = requests.get(htm)
 
-    res = requests.get(ht)
-
-    html = BeautifulSoup(res.content, 'html.parser')
+        html = BeautifulSoup(res.content, 'html.parser')
 
   
-    data_r = []
+        data_r = []
 
-    datah1 = html.find_all('h1') #'h1' tag
-    datah2 = html.find_all('h2') #'h2' tag
-    datah3 = html.find_all('h3') #'h3' tag
-    datah4 = html.find_all('h4') #'h4' tag
-    datap = html.find_all('p') #'p' tag
-    datali = html.find_all('li') #'li' tag
+        datah1 = html.find_all('h1') #'h1' tag
+        datah2 = html.find_all('h2') #'h2' tag
+        datah3 = html.find_all('h3') #'h3' tag
+        datah4 = html.find_all('h4') #'h4' tag
+        datap = html.find_all('p') #'p' tag
+        datali = html.find_all('li') #'li' tag
 
-    result = []
+        result = []
 
-    for n in range(len(datah1)):
-        data_r.extend(datah1[n].get_text().lower().split())
-    for n in range(len(datah2)):
-        data_r.extend(datah2[n].get_text().lower().split())
-    for n in range(len(datah3)):
-        data_r.extend(datah3[n].get_text().lower().split())
-    for n in range(len(datah4)):
-        data_r.extend(datah4[n].get_text().lower().split())
-    for n in range(len(datap)):
-        data_r.extend(datap[n].get_text().lower().split())
-    for n in range(len(datali)):
-        data_r.extend(datali[n].get_text().lower().split())
+        for n in range(len(datah1)):
+            data_r.extend(datah1[n].get_text().lower().split())
+        for n in range(len(datah2)):
+            data_r.extend(datah2[n].get_text().lower().split())
+        for n in range(len(datah3)):
+            data_r.extend(datah3[n].get_text().lower().split())
+        for n in range(len(datah4)):
+            data_r.extend(datah4[n].get_text().lower().split())
+        for n in range(len(datap)):
+            data_r.extend(datap[n].get_text().lower().split())
+        for n in range(len(datali)):
+            data_r.extend(datali[n].get_text().lower().split())
 
     
-    text =" ".join(data_r)
+        text =" ".join(data_r)
 
-    line = re.sub('[-=+,#/\?:^$.@©*;\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', text)
+        line = re.sub('[-=+,#/\?:^$.@©*;\"※~&%ㆍ!』\\‘|\(\)\[\]\<\>`\'…》]', '', text)
     
-    freq = []
-    result.extend(line.split())
+        freq = []
+        result.extend(line.split())
 
-    for i in range(len(result)):
-        freq.append(result.count(result[i]))
+        for i in range(len(result)):
+            freq.append(result.count(result[i]))
 
-    dic = {}
-    for k in range(len(result)):
-        dic[result[k]] = freq[k]  
-    print(len(dic))
+        dic = {}
+        for k in range(len(result)):
+            dic[result[k]] = freq[k]
+        
+        return htm
 
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
